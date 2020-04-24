487a72ce0983a419e79b21d210223461e475d507
pasha29 <pasha4383@gmail.com>
2020-04-24 23:06:45 +0300

    Updates

---
+++ precache-manifest.6c6276871c18b4a7e1dc784a093e223b.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba126481f0b23937b6919abd0b2b10c5",
+    "url": "/tasklingerie/index.html"
+  },
+  {
+    "revision": "7ecde2bd1fff1b088e1d",
+    "url": "/tasklingerie/static/css/2.382c35bb.chunk.css"
+  },
+  {
+    "revision": "261d3732b530654ccca5",
+    "url": "/tasklingerie/static/css/main.d66d982a.chunk.css"
+  },
+  {
+    "revision": "7ecde2bd1fff1b088e1d",
+    "url": "/tasklingerie/static/js/2.93c35cdd.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/tasklingerie/static/js/2.93c35cdd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "261d3732b530654ccca5",
+    "url": "/tasklingerie/static/js/main.db5d4473.chunk.js"
+  },
+  {
+    "revision": "ecb32097d599ab13ebef",
+    "url": "/tasklingerie/static/js/runtime-main.8bc8b7b6.js"
+  },
+  {
+    "revision": "f723f0d891db897a861121b0aff23218",
+    "url": "/tasklingerie/static/media/Schnyder_X_Cond_XL_Demi.f723f0d8.otf"
+  },
+  {
+    "revision": "2972fbe663744db45eb8a7aa06541f61",
+    "url": "/tasklingerie/static/media/Schnyder_X_Cond_XL_Light.2972fbe6.otf"
+  },
+  {
+    "revision": "2dc2ec6483359bb022c9a91c2994b0de",
+    "url": "/tasklingerie/static/media/TTTravels-Regular.2dc2ec64.ttf"
+  },
+  {
+    "revision": "0da9d3c4b4cc0a0ee836ecb99bf71699",
+    "url": "/tasklingerie/static/media/header_logo.0da9d3c4.svg"
+  },
+  {
+    "revision": "9162098037addcfd4cc7e1299989c7ff",
+    "url": "/tasklingerie/static/media/image1.91620980.png"
+  },
+  {
+    "revision": "23476a9020a4397b4c98db0b53da164d",
+    "url": "/tasklingerie/static/media/image2.23476a90.png"
+  },
+  {
+    "revision": "9cbc856d2cd9408adac2a75f207ca40c",
+    "url": "/tasklingerie/static/media/image3.9cbc856d.png"
+  },
+  {
+    "revision": "06a4fc7fa494c8c6c12a83e1a267a098",
+    "url": "/tasklingerie/static/media/main_bg.06a4fc7f.svg"
+  }
+]);
